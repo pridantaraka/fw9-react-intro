6553dbc285103ae4c900c170c44278e0aa11e468
pridantaraka <pridantaraka@gmail.com>
2022-07-20 21:00:33 +0700

    update count

--- src/App.js
+++ src/App.js
@@ -1,34 +1,18 @@
 import React from "react";
-// import { Row } from "react-bootstrap";
 import { Col,Row } from "react-bootstrap";
+import {Button} from "react-bootstrap";
 
 const App = () =>{
-  const [add,Hasil] = React.useState(0)
-React.useEffect(()=>{
-    console.log(add);
-  }, [add])
+  const [add,hasil] = React.useState(0)
   return (
-    <>
-    <Row className="">
-      <Col className="d-flex col-12 justify-content-betwen">
-      <Col className="d-flex align-self-center"><button onClick={()=>Hasil(add-1)}>Kurang</button></Col>
-      <Col className="d-flex align-self-center"><h1>{add}</h1></Col>
-      <Col className="d-flex align-self-center"><button onClick={()=>Hasil(add+1)}>Tambah</button></Col>
-      </Col>
-    </Row>
-    </>
+    <main className="min-vh-100 d-flex justify-content-center">
+      <Row>
+        <Col className="d-flex align-self-center"><Button disabled={add<1} onClick={()=>hasil(add-1)}>Kurang</Button></Col>
+        <Col className="d-flex align-self-center"><h1>{add}</h1></Col>
+        <Col className="d-flex align-self-center"><Button disabled={add>=10} onClick={()=>hasil(add+1)}>Tambah</Button></Col>
+      </Row>
+    </main>
+    
 )
 }
-
-// const App = ()=> {
-//   // const [isShowed, toggleShow] =React.useState(true)
-//   return(
-//     <ol>
-//       {/* {isShowed && <Series name={{episode:5}}>Text</Series>} */}
-
-//       {/* <button onClick={()=>toggleShow(!isShowed)}></button> */}
-//     </ol>
-//   )
-// }
-
 export default App
